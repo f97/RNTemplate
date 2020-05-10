5489759b7ee46be965109dc238d099a7cdb4eb0a
Huỳnh Đức Khoản <huynhduckhoan@Huynhs-MacBook-Pro.local>
2020-05-10 10:58:38 +0700

    Init commit

---
+++ App/Theme/Images.js
@@ -0,0 +1,7 @@
+/**
+ * Images should be stored in the `App/Images` directory and referenced using variables defined here.
+ */
+
+export default {
+  logo: require('https://mrsoi.com/image/avatar.jpg'),
+}
